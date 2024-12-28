a47b29bcce35facbb17e64bfd6d389cfb7314786
Rahulkumar Gupta <159034627+grahulkumar@users.noreply.github.com>
2024-12-28 21:16:10 +0530

    minor changes

--- jinglebytex-frontend/app/page.tsx
+++ jinglebytex-frontend/app/page.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-white dark:bg-gray-900">
+    <div className="flex flex-col items-center justify-center bg-white dark:bg-gray-900">
       <h1 className="text-4xl font-bold text-gray-900 dark:text-gray-100">
         Hello, Dark Mode!
       </h1>
